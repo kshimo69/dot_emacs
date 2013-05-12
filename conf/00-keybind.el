d17af06833c8d56cadf820e2c70d4b700f7abd7a
Kimihiko Shimomura <kshimo69@gmail.com>
2013-05-12 17:50:51 +0900

    add C-m

--- conf/00-keybind.el
+++ conf/00-keybind.el
@@ -3,6 +3,9 @@
 ;; C-hをバックスペースに
 (global-set-key (kbd "C-h") 'delete-backward-char)
 
+;;改行後に自動インデント
+(global-set-key "\C-m" 'newline-and-indent)
+
 ;; Scroll buffer without moving the cursor
 (global-set-key (kbd "M-p") '(lambda () (interactive) (scroll-down 1)))
 (global-set-key (kbd "M-n") '(lambda () (interactive) (scroll-up 1)))
