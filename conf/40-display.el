5bf6fc9348d8684649b14581a8d2fbe653d6790d
Kimihiko Shimomura <kshimo69@gmail.com>
2013-05-12 23:12:46 +0900

    disable which-func-mode

--- conf/40-display.el
+++ conf/40-display.el
@@ -1,10 +1,21 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
-;; 現在の関数名を画面の上に表示する
-(which-func-mode 1)
-;; すべてのメジャーモードにwhich-func-modeを適用する
-(setq which-func-modes t)
-;; 画面上部に表示する
-(delete (assoc 'which-func-mode mode-line-format) mode-line-format)
-(setq-default header-line-format '(which-func-mode ("" which-func-format)))
-(set-face-foreground 'which-func "pink")
+;; ;; 現在の関数名を画面の上に表示する
+;; (which-func-mode 1)
+;; ;; すべてのメジャーモードにwhich-func-modeを適用する
+;; (setq which-func-modes t)
+;; ;; 画面上部に表示する
+;; (delete (assoc 'which-func-mode mode-line-format) mode-line-format)
+;; (setq-default header-line-format '(which-func-mode ("" which-func-format)))
+;; (set-face-foreground 'which-func "pink")
+
+(defun toggle-which-func-mode ()
+  (interactive)
+  (which-func-mode)
+  (if which-func-mode
+      (progn
+        (delete (assoc 'which-func-mode mode-line-format)
+                mode-line-format)
+        (setq-default header-line-format
+                      '(which-func-mode ("" which-func-format))))
+    (setq-default header-line-format nil)))
