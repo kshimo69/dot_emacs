64aff0499ad8c6406c89f7325c37fe938f488104
Shimomura Kimihiko <kimihiko_shimomura@nintendo.co.jp>
2013-05-13 13:13:51 +0900

    disable ping in auto-install

--- conf/30-auto-install.el
+++ conf/30-auto-install.el
@@ -5,6 +5,7 @@
 ;; http://www.emacswiki.org/emacs/AutoInstall
 (when (require 'auto-install nil t)
   (setq auto-install-directory "~/.emacs.d/plugins/")
-  (auto-install-update-emacswiki-package-name t)
+  (setq auto-install-use-wget nil)
+  ;; (auto-install-update-emacswiki-package-name t)
   (setq ediff-window-setup-function 'ediff-setup-windows-plain)
   )
