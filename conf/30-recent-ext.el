e07e8ffd1926b9b594887bc1a2cf2a96aa046b63
Shimomura Kimihiko <kimihiko_shimomura@nintendo.co.jp>
2013-05-13 19:50:00 +0900

    auto save for recentf

--- conf/30-recent-ext.el
+++ conf/30-recent-ext.el
@@ -3,6 +3,14 @@
 ;; 保存先変更
 (set-default 'recentf-save-file "~/.emacs.d/var/recentf")
 
+(when (require 'recentf nil t)
+  (setq recentf-max-saved-items 2000)
+  (setq recentf-exclude '("recentf"))
+  (setq recentf-auto-cleanup 10)
+  (setq recentf-auto-save-timer
+        (run-with-idle-timer 30 t 'recentf-save-list))
+  (recentf-mode 1))
+
 ;; 履歴一覧を開く
 ;; (auto-install-from-emacswiki "recentf-ext.el")
 (when (require 'recentf-ext nil t)
