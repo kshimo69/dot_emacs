b1d41b335e94704c9d029d3796b0bbcc0e56ff5e
Shimomura Kimihiko <kimihiko_shimomura@nintendo.co.jp>
2013-05-16 08:40:40 +0900

    change tab to space

--- conf/20-package.el
+++ conf/20-package.el
@@ -30,8 +30,8 @@
 ;;     ))
 
 ;; (let ((not-installed (loop for x in installing-package-list
-;; 			   when (not (package-installed-p x))
-;; 			   collect x)))
+;;                            when (not (package-installed-p x))
+;;                            collect x)))
 ;;   (when not-installed
 ;;     (package-refresh-contents)
 ;;     (dolist (pkg not-installed)
