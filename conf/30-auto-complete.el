2484c8bddaba38ff62bda414f5a06daed121c645
Kimihiko Shimomura <kshimo69@gmail.com>
2013-05-18 01:22:39 +0900

    change ac save file

--- conf/30-auto-complete.el
+++ conf/30-auto-complete.el
@@ -8,6 +8,9 @@
                              "Kimihiko Shimomura"
                              ))
 
+  ;; historyの保存先
+  (setq ac-comphist-file (expand-file-name "~/.emacs.d/var/ac-comphist.dat"))
+
   ;; 補完メニュー表示時のみC-n/C-pで補完候補を選択
   (setq ac-use-menu-map t)
   ;; デフォルトで設定済み
@@ -36,8 +39,8 @@
 
   ;; http://d.hatena.ne.jp/kitokitoki/20100627/p1
   ;; リージョンを開いているバッファのメジャーモードの辞書へ追加する
-  (defvar auto-complete-dict-path "~/.emacs.d/ac-dict/")
-  (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict/")
+  (defvar auto-complete-dict-path "~/.emacs.d/share/ac-dict/")
+  (add-to-list 'ac-dictionary-directories "~/.emacs.d/share/ac-dict/")
   (defun append-region-to-auto-complete-dict ()
     (interactive)
     (when (and transient-mark-mode mark-active)
