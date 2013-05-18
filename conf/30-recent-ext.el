6a67faf62f9784ff31b6d2b91aaacc505b6fe97c
Kimihiko Shimomura <kshimo69@gmail.com>
2013-05-18 14:04:24 +0900

    change recentf setting

--- conf/30-recent-ext.el
+++ conf/30-recent-ext.el
@@ -5,11 +5,25 @@
 
 (when (require 'recentf nil t)
   (setq recentf-max-saved-items 2000)
-  (setq recentf-exclude '("recentf"))
-  (setq recentf-auto-cleanup 10)
+  (setq recentf-auto-cleanup 30)
   (setq recentf-auto-save-timer
-        (run-with-idle-timer 30 t 'recentf-save-list))
-  (recentf-mode 1))
+        (run-with-idle-timer 300 t 'recentf-save-list))
+  ;; 保存ファイルの設定に リモートファイル tramp の先等を追加。
+  ;; これを実施すると起動時にパスワード等の確認はされない
+  (add-to-list 'recentf-keep 'file-remote-p)
+  (add-to-list 'recentf-keep 'file-readable-p)
+  ;; 除外ファイル
+  (setq recentf-exclude
+        '("\\.elc$"
+          "\\.pyc$"
+          "\\.cache$"
+          "recentf$"
+          "revive$"
+          ))
+  ;; Emacs 終了時に cleanup
+  (add-hook 'kill-emacs-query-functions 'recentf-cleanup)
+  (recentf-mode 1)
+)
 
 ;; 履歴一覧を開く
 ;; (auto-install-from-emacswiki "recentf-ext.el")
