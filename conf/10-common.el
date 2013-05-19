0617eb8088d41d6616cf31a7e5e55918f6cadb29
Kimihiko Shimomura <kshimo69@gmail.com>
2013-05-19 23:00:37 +0900

    change server-start setting

--- conf/10-common.el
+++ conf/10-common.el
@@ -39,7 +39,10 @@
 (if window-system
     (progn
       (require 'server)
-      (unless (server-running-p) (server-start))
+      (when (and (functionp 'server-running-p)
+                 (not (server-running-p)))
+        (server-start))
+      ;; (unless (server-running-p) (server-start))
       (defun skt:raise-frame()
         ;; Frame を前面にする
         (raise-frame (selected-frame))
