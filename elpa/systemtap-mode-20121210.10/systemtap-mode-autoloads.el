b09754a07ca5720fad1d678f77b8f270cdcf4080
Kimihiko Shimomura <kshimo69@gmail.com>
2013-05-19 20:54:54 +0900

    add systemtap-mode

---
+++ elpa/systemtap-mode-20121210.10/systemtap-mode-autoloads.el
@@ -0,0 +1,34 @@
+;;; systemtap-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (systemtap-mode) "systemtap-mode" "systemtap-mode.el"
+;;;;;;  (20888 48316 0 0))
+;;; Generated autoloads from systemtap-mode.el
+
+(add-to-list 'auto-mode-alist '("\\.stp\\'" . systemtap-mode))
+
+(autoload 'systemtap-mode "systemtap-mode" "\
+Major mode for editing SystemTap scripts.
+
+Key bindings:
+\\{systemtap-mode-map}
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("systemtap-mode-pkg.el") (20888 48316
+;;;;;;  477491 0))
+
+;;;***
+
+(provide 'systemtap-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; systemtap-mode-autoloads.el ends here
---
+++ elpa/systemtap-mode-20121210.10/systemtap-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "systemtap-mode" "20121210.10" "A mode for SystemTap [github]" (quote nil))
